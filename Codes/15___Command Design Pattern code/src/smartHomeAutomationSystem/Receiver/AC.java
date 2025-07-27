cbd3b5c8b67d67ee65303fc68e2c4506d0276a52
Sibasundar Jena <sibasundarj8@gmail.com>
2025-07-27 23:27:16 +0530

    Create AC.java

---
+++ Codes/15___Command Design Pattern code/src/smartHomeAutomationSystem/Receiver/AC.java
@@ -0,0 +1,10 @@
+package smartHomeAutomationSystem.Receiver;
+
+public class AC {
+    public void on() {
+        System.out.println("Turned ON :: AC");
+    }
+    public void off() {
+        System.out.println("Turned OFF :: AC");
+    }
+}
