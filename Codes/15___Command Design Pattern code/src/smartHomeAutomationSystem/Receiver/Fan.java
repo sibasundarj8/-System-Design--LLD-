206b9c785d82f556b9688afa22924dee8bb4a299
Sibasundar Jena <sibasundarj8@gmail.com>
2025-07-27 23:28:24 +0530

    Add files via upload

---
+++ Codes/15___Command Design Pattern code/src/smartHomeAutomationSystem/Receiver/Fan.java
@@ -0,0 +1,10 @@
+package smartHomeAutomationSystem.Receiver;
+
+public class Fan {
+    public void start() {
+        System.out.println("Starting :: Fan");
+    }
+    public void stop() {
+        System.out.println("Stoping :: Fan");
+    }
+}
---
+++ Codes/15___Command Design Pattern code/src/smartHomeAutomationSystem/Receiver/Light.java
@@ -0,0 +1,10 @@
+package smartHomeAutomationSystem.Receiver;
+
+public class Light {
+    public void on() {
+        System.out.println("Turned ON :: Light");
+    }
+    public void off() {
+        System.out.println("Turned OFF :: Light");
+    }
+}
