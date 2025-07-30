bbeebe0902a264267507efd8ff5f611965acd75d
Sibasundar Jena <sibasundarj8@gmail.com>
2025-07-30 18:44:12 +0530

    Create Client.java

---
+++ Codes/17___Facade Design Pattern code/src/Client.java
@@ -0,0 +1,8 @@
+import Computer.ComputerFacade;
+
+public class Client {
+    public static void main(String[] args) {
+        ComputerFacade computerFacade = new ComputerFacade();
+        computerFacade.startComputer();
+    }
+}
