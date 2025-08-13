c27acf0267c9daa9e5de957ae94fe6618d2cd8a7
Sibasundar Jena <sibasundarj8@gmail.com>
2025-08-13 17:31:00 +0530

    Create BluetoothSpeakerAPI.java

---
+++ Projects/SoundBloom Music player App/src/soundbloom/external/BluetoothSpeakerAPI.java
@@ -0,0 +1,11 @@
+package soundbloom.external;
+
+public class BluetoothSpeakerAPI {
+    public String playBluetoothSpeaker() {
+        return "▶️ [Bluetooth] playing music: ";
+    }
+
+    public String pauseBluetoothSpeaker() {
+        return "⏸️ [Bluetooth] pausing music: ";
+    }
+}
