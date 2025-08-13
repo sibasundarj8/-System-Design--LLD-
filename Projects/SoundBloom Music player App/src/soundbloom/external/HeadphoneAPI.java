27c3018e003740f5f601722466cdb6e190203253
Sibasundar Jena <sibasundarj8@gmail.com>
2025-08-13 17:36:10 +0530

    Add files via upload

---
+++ Projects/SoundBloom Music player App/src/soundbloom/external/HeadphoneAPI.java
@@ -0,0 +1,11 @@
+package soundbloom.external;
+
+public class HeadphoneAPI {
+    public String playHeadphone() {
+        return "▶️ [Headphone] playing music: ";
+    }
+
+    public String pauseHeadphone() {
+        return "⏸️ [Headphone] pausing music: ";
+    }
+}
---
+++ Projects/SoundBloom Music player App/src/soundbloom/external/WiredSpeakerAPI.java
@@ -0,0 +1,11 @@
+package soundbloom.external;
+
+public class WiredSpeakerAPI {
+    public String playWiredSpeaker() {
+        return "▶️ [Wired speaker] playing music: ";
+    }
+
+    public String pauseWiredSpeaker() {
+        return "⏸️ [Wired speaker] pausing music: ";
+    }
+}
